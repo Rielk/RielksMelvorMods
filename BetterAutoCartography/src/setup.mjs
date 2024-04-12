0198a8f878f062b1c4e6d6446c08efbf4d788f12
William Sullivan <william@symbotics.co.uk>
2024-04-12 03:10:31 -0400

    Sped up Survey Interval for testing.

--- BetterAutoCartography/src/setup.mjs
+++ BetterAutoCartography/src/setup.mjs
@@ -1,6 +1,8 @@
 export async function setup(ctx) {
     const { betterGetNextAutoSurveyHex } = await ctx.loadModule('src/betterGetNextAutoSurveyHex.mjs');
     ctx.patch(Cartography, 'getNextAutoSurveyHex').after((_, hex, nextHexes = []) => betterGetNextAutoSurveyHex(hex, nextHexes));
+
+    ctx.patch(Cartography, 'surveyInterval').get((old_val) => old_val() / 100);
 }
 
 // getNextAutoSurveyHexes in cartography
