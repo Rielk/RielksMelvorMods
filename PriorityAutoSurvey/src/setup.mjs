a5badb12467d11bb3c9d4e235a5847952ff14e23
William Sullivan <william@symbotics.co.uk>
2024-04-13 03:21:51 -0400

    Turned cheats back on

--- PriorityAutoSurvey/src/setup.mjs
+++ PriorityAutoSurvey/src/setup.mjs
@@ -2,6 +2,7 @@ export async function setup(ctx) {
     const { betterGetNextAutoSurveyHex } = await ctx.loadModule('src/betterGetNextAutoSurveyHex.mjs');
     ctx.patch(Cartography, 'getNextAutoSurveyHex').after((_, hex, nextHexes = []) => betterGetNextAutoSurveyHex(hex, nextHexes));
 
-    //ctx.patch(Cartography, 'surveyInterval').get((old_val) => old_val() / 100);
-    //ctx.patch(WorldMap, 'sightRange').get(_ => 1000);
+    ctx.patch(Cartography, 'surveyInterval').get(_ => 50);
+    ctx.patch(WorldMap, 'sightRange').get(_ => 3);
+    ctx.patch(WorldMap, 'surveyRange').get(_ => 2);
 }
