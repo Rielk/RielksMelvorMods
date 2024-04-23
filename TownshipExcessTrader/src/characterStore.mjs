bf7961b08bc6f049de529635fd63f6d6923215a5
William Sullivan <william@symbotics.co.uk>
2024-04-23 02:04:16 -0400

    Insert any control in the correct place.

---
+++ TownshipExcessTrader/src/characterStore.mjs
@@ -0,0 +1,2 @@
+export function onCharacterLoaded(ctx) {
+}
--- TownshipExcessTrader/src/setup.mjs
+++ TownshipExcessTrader/src/setup.mjs
@@ -1,6 +1,37 @@
 export async function setup(ctx) {
-    ctx.patch(Township, 'tick').before(getNextAutoSurveyHexPatches.beforePatch);
+    ctx.onCharacterLoaded((await ctx.loadModule('src/characterStore.mjs')).onCharacterLoaded);
 
-    //game.township.resources.forEach((resource)=>{
-    // let amount = resource.generation;
+    const automaticTrader = await ctx.loadModule('src/automaticTrader.mjs');
+    ctx.patch(Township, 'tick').after(automaticTrader.afterPatch);
+
+    const uiMine = await ctx.loadModule('src/ui.mjs');
+    ctx.loadTemplates('src/templates.html');
+    ctx.onInterfaceReady(() => {
+        game.township.resources.forEach((_, resource) => {
+            const element = document.getElementById(`jump-to-resource-${resource}`);
+            if (element) {
+                const id = `auto-trade-from-resource-${resource}`;
+                const uiElement = uiMine.followWithList(element, id, 'nav-main nav-main-horizontal nav-main-horizontal-override font-w400 font-size-sm mb-2 auto-convert-from-township')
+                ui.create(Counter({ count: 0 }), uiElement);
+            }
+        });
+    });
+
+    ctx.onCharacterLoaded(() => {
+        game.township.PASSIVE_TICK_LENGTH = 5;
+        game.township.tickTimer._maxTicks = 5;
+        game.township.tickTimer._ticksLeft = 5;
+        game.township.tickTimer.tick();
+    });
+    ctx.patch(Township, 'tick').after(() => game.gp.set(1000000000));
+}
+
+function Counter(props) {
+    return {
+        $template: '#counter-component',
+        count: props.count,
+        inc() {
+            this.count++;
+        }
+    };
 }
