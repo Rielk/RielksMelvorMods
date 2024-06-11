552eefc1f2d9aa06c6b8e4e233ea75632ab9d95a
William Sullivan <william@symbotics.co.uk>
2024-06-10 23:37:28 -0400

    Updated TownshipAutoTrader to 1.3

--- TownshipAutoTrader/src/automaticTrader.mjs
+++ TownshipAutoTrader/src/automaticTrader.mjs
@@ -17,99 +17,99 @@ export function autoTrade(config, resources) {
 function autoTradeResource(config, resource) {
     //Get current states
     const conversions = config.enabledConversionsForResource(resource.id);
-    const pendingItems = [];
+    const pendingConversions = [];
     conversions.forEach(conversion => {
         if (!isRequirementMet(conversion.unlockRequirements))
             return;
 
         const item = conversion.item;
-        const ratio = game.township.getBaseConvertFromTownshipRatio(resource, item);
+        const ratio = game.township.getBaseConvertFromTownshipRatio(conversion);
         const currentQuantity = getQuantityOfItemAndUpgrades(item);
 
-        pendingItems.push({
-            item: item,
+        pendingConversions.push({
+            conversion: conversion,
             currentQuantity: currentQuantity,
             ratio: ratio,
             amountToAdd: 0
         });
     });
-    if (pendingItems.length === 0)
+    if (pendingConversions.length === 0)
         return;
 
     //Calculate Spending
     const tradeMode = config.getResourceTradeMode(resource.id);
     let resourceAtStart = Math.max(0, resource.amount - config.getResourceLimit(resource.id));
-    var buyingItems;
+    var buyingConversions;
     switch (tradeMode) {
         case config.TradeModes.buyEqualCost:
-            buyingItems = equalCostBuying(resourceAtStart, pendingItems);
+            buyingConversions = equalCostBuying(resourceAtStart, pendingConversions);
             break;
         case config.TradeModes.buyEqualQuantity:
-            buyingItems = equalQuantityBuying(resourceAtStart, pendingItems);
+            buyingConversions = equalQuantityBuying(resourceAtStart, pendingConversions);
             break;
         default:
         case config.TradeModes.lowestQuantity:
-            buyingItems = lowestQuantityBuying(resourceAtStart, pendingItems);
+            buyingConversions = lowestQuantityBuying(resourceAtStart, pendingConversions);
             break;
     }
 
     //Commit Purchases
-    buyingItems.forEach(i => {
-        if (i.amountToAdd <= 0)
+    buyingConversions.forEach(c => {
+        if (c.amountToAdd <= 0)
             return;
-        game.township.convertQty = Math.floor(i.amountToAdd);
-        game.township.processConversionFromTownship(i.item, resource);
+        game.township.convertQty = Math.floor(c.amountToAdd);
+        game.township.processConversionFromTownship(c.conversion, resource);
     });
 }
 
-function equalCostBuying(resourceAtStart, pendingItems) {
-    const resourcePerItem = Math.floor(resourceAtStart / pendingItems.length);
+function equalCostBuying(resourceAtStart, pendingConversions) {
+    const resourcePerItem = Math.floor(resourceAtStart / pendingConversions.length);
 
-    const buyingItems = [];
-    while (pendingItems.length > 0) {
-        const item = pendingItems.pop();
+    const buyingConversions = [];
+    while (pendingConversions.length > 0) {
+        const item = pendingConversions.pop();
         const amountToBuy = item.ratio === 0 ? 0 : Math.floor(resourcePerItem / item.ratio);
         item.amountToAdd = amountToBuy;
-        buyingItems.push(item);
+        buyingConversions.push(item);
     }
-    return buyingItems;
+    return buyingConversions;
 }
 
-function equalQuantityBuying(resourceAtStart, pendingItems) {
-    const totalRatio = pendingItems.reduce((a, i) =>  a + i.ratio , 0);
+function equalQuantityBuying(resourceAtStart, pendingConversions) {
+    const totalRatio = pendingConversions.reduce((a, i) =>  a + i.ratio , 0);
     const maxCanBuy = totalRatio === 0 ? 0 : Math.floor(resourceAtStart / totalRatio);
 
-    const buyingItems = [];
-    while (pendingItems.length > 0) {
-        const item = pendingItems.pop();
+    const buyingConversions = [];
+    while (pendingConversions.length > 0) {
+        const item = pendingConversions.pop();
         item.amountToAdd = maxCanBuy;
-        buyingItems.push(item);
+        buyingConversions.push(item);
     }
-    return buyingItems;
+    return buyingConversions;
 }
 
-function lowestQuantityBuying(resourceAtStart, pendingItems) {
-    pendingItems.sort((a, b) => b.currentQuantity - a.currentQuantity);
+function lowestQuantityBuying(resourceAtStart, pendingConversions) {
+    pendingConversions.sort((a, b) => b.currentQuantity - a.currentQuantity);
 
     let resourceToSpend = resourceAtStart;
-    const buyingItems = [];
-    while (pendingItems.length > 0) {
-        buyingItems.push(pendingItems.pop());
-        const totalRatio = buyingItems.reduce((a, i) => a + i.ratio, 0);
-        const qtyLimit = pendingItems.slice(-1)[0]?.currentQuantity; //Will be undefined on final pass
-        const currentTotal = buyingItems[0].currentQuantity + buyingItems[0].amountToAdd;
+    const buyingConversions = [];
+    while (pendingConversions.length > 0) {
+        buyingConversions.push(pendingConversions.pop());
+        const totalRatio = buyingConversions.reduce((a, i) => a + i.ratio, 0);
+        const qtyLimit = pendingConversions.slice(-1)[0]?.currentQuantity; //Will be undefined on final pass
+        const currentTotal = buyingConversions[0].currentQuantity + buyingConversions[0].amountToAdd;
         const maxCanBuy = totalRatio === 0 ? 0 : Math.floor(resourceToSpend / totalRatio);
         var qtyToBuy;
         if (qtyLimit === undefined)
             qtyToBuy = maxCanBuy;
         else
             qtyToBuy = Math.min(maxCanBuy, qtyLimit - currentTotal);
-        buyingItems.forEach(i => i.amountToAdd += qtyToBuy);
+        buyingConversions.forEach(i => i.amountToAdd += qtyToBuy);
         resourceToSpend -= qtyToBuy * totalRatio;
     }
 
     //Buy Individuals
-    for (const i of buyingItems) {
+    for (const i of buyingConversions) {
         if (i.ratio < resourceToSpend) {
             resourceToSpend -= i.ratio;
             i.amountToAdd += 1;
@@ -117,7 +117,7 @@ function lowestQuantityBuying(resourceAtStart, pendingItems) {
         else
             break;
     };
-    return buyingItems;
+    return buyingConversions;
 }
 
 function getQuantityOfItemAndUpgrades(item) {
