3cd63b44ba978d7f2d6ebef36728c911d2b0533e
William Sullivan <william@symbotics.co.uk>
2024-09-11 18:37:54 -0400

    It loads again!

--- Construction/src/construction/construction.mjs
+++ Construction/src/construction/construction.mjs
@@ -1,17 +1,101 @@
-export class Construction extends Skill {
-    //To add action, add passiveTick or activeTick property to Construction class.
-
+export class Construction extends ArtisanSkill {
     constructor(namespace, game) {
-        super(namespace, 'Construction', game);
-
+        super(namespace, 'Construction', game, ConstructionRecipe.name);
         this._media = 'assets/icon.png';
-        this.renderQueue = new ConstructionRenderQueue();
-        this.isActive = false;
+        this.baseInterval = 3000;
+        this.UI = undefined;
+        this.renderQueue = new ArtisanSkillRenderQueue();
+        this.categories = new NamespaceRegistry(game.registeredNamespaces, 'ConstructionCategory');
+        this.subcategories = new NamespaceRegistry(game.registeredNamespaces,'ConstructionSubcategory');
+    }
+
+    isMasteryActionUnlocked(action) {
+        return this.isBasicSkillRecipeUnlocked(action);
+    }
+
+    get selectionTabs() {
+        return this.UI.constructionSelectionTabs;
+    }
+
+    get menu() {
+        return this.UI.constructionArtisanMenu;
+    }
+    get categoryMenu() {
+        return this.UI.constructionCategoryMenu;
+    }
+    get noCostsMessage() {
+        return getLangString('TOASTS_MATERIALS_REQUIRED_TO_CRAFT');
+    }
+    get actionItem() {
+        return this.activeRecipe.product;
+    }
+    get unmodifiedActionQuantity() {
+        return this.activeRecipe.baseQuantity;
+    }
+    get activeRecipe() {
+        if (this.selectedRecipe === undefined)
+            throw new Error('Tried to get active crafting recipe, but none is selected.');
+        return this.selectedRecipe;
+    }
+    get masteryModifiedInterval() {
+        return 1700;
+    }
+
+    registerData(namespace, data) {
+        var _a, _b, _c;
+        (_a = data.categories) === null || _a === void 0 ? void 0 : _a.forEach((categoryData)=>{
+            this.categories.registerObject(new ConstructionCategory(namespace,categoryData,this,this.game));
+        }
+        );
+        (_b = data.subcategories) === null || _b === void 0 ? void 0 : _b.forEach((subcategoryData)=>{
+            this.subcategories.registerObject(new SkillSubcategory(namespace,subcategoryData));
+        }
+        );
+        (_c = data.recipes) === null || _c === void 0 ? void 0 : _c.forEach((recipeData)=>{
+            this.actions.registerObject(new ConstructionRecipe(namespace,recipeData,this.game,this));
+        }
+        );
+        super.registerData(namespace, data);
+    }
+
+    getRegistry(type) {
+        switch (type) {
+        case ScopeSourceType.Category:
+            return this.categories;
+        case ScopeSourceType.Action:
+            return this.actions;
+        case ScopeSourceType.Subcategory:
+            return this.subcategories;
+        }
     }
 }
 
-class ConstructionRenderQueue extends SkillRenderQueue {
-    constructor() {
-        super(...arguments);
+class ConstructionCategory extends SkillCategory {
+    constructor(namespace, data, skill, game) {
+        super(namespace, data, skill, game);
+        this.type = data.type;
+        if (this.type == 'Room')
+            this.room = data.room
+    }
+}
+
+class ConstructionRecipe extends SingleProductArtisanSkillRecipe {
+    constructor(namespace, data, game, skill) {
+        super(namespace, data, game, skill);
+        try {
+            if (data.subcategoryID !== undefined)
+                this.subcategory = skill.subcategories.getObjectSafe(data.subcategoryID);
+        } catch (e) {
+            throw new DataConstructionError(ConstructionRecipe.name,e,this.id);
+        }
+    }
+    applyDataModification(data, game) {
+        super.applyDataModification(data, game);
+        try {
+            if (data.subcategoryID !== undefined)
+                this.subcategory = game.smithing.subcategories.getObjectSafe(data.subcategoryID);
+        } catch (e) {
+            throw new DataModificationError(ConstructionRecipe.name,e,this.id);
+        }
     }
 }
--- Construction/src/interface/constructionInterface.mjs
+++ Construction/src/interface/constructionInterface.mjs
@@ -3,10 +3,57 @@ export class ConstructionInterface {
         this.ctx = ctx;
         this.construction = construction;
 
+        this.constructionSelectionTabs = new Map();
+
         this.ctx.onInterfaceAvailable(async () => {
-            this.ctx.loadStylesheet('src/interface/construction-styles.css');
-            this._content = getTemplateNode('rielk-construction');
-            document.getElementById('main-container').append(this._content);
+            await this.ctx.loadStylesheet('src/interface/construction-styles.css');
+
+            const frag = new DocumentFragment();
+            frag.append(getTemplateNode('rielk-construction-template'));
+            document.getElementById('main-container').append(...frag.children);
+            this.constructionCategoryMenu = document.getElementById('construction-category-menu');
+            this.constructionArtisanMenu = document.getElementById('construction-artisan-menu',);
+        });
+
+        this.ctx.onCharacterLoaded(async () => {
+            this.constructionCategoryMenu.addOptions(game.construction.categories.allObjects, getLangString('MENU_TEXT_SELECT_CONSTRUCTION_CATEGORY'), switchToCategory(this.constructionSelectionTabs));
+            this.constructionArtisanMenu.init(game.construction);
+            const constructionCategoryContainer = document.getElementById('construction-category-container');
+            this.construction.categories.forEach((category) => {
+                const recipes = game.construction.actions.filter((r) => r.category === category);
+                recipes.sort(BasicSkillRecipe.sortByLevels);
+                const tab = createElement('recipe-selection-tab', {
+                    className: 'col-12 col-md-8 d-none',
+                    attributes: [['data-option-tag-name', 'construction-recipe-option']],
+                    parent: constructionCategoryContainer,
+                });
+                tab.setRecipes(recipes, game.construction);
+                this.constructionSelectionTabs.set(category, tab);
+            });
         });
     }
+
+    switchConstructionCategory(category) {
+        switch (category.type) {
+            case 'Creation':
+                showElement();
+                hideElement();
+                break;
+            case 'Room':
+                showElement();
+                hideElement();
+                break;
+        }
+    }
 }
+
+class ConstructionRecipeOptionElement extends ItemRecipeOptionElement {
+    setUnlocked(recipe) {
+        super.setUnlocked(recipe);
+        this.unlocked.onclick = ()=>game.construction.selectRecipeOnClick(recipe);
+    }
+    getRecipeIngredients(recipe) {
+        return game.construction.getRecipeCosts(recipe);
+    }
+}
+window.customElements.define('construction-recipe-option', ConstructionRecipeOptionElement);
