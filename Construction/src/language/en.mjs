1a99563b05cb62ce33d4e6e4208876e074669b6c
William Sullivan <william@symbotics.co.uk>
2024-10-01 08:33:19 -0400

    Fix to modifier lang strings and added other strings

--- Construction/src/language/en.mjs
+++ Construction/src/language/en.mjs
@@ -11,6 +11,12 @@ export const language = {
     'MENU_TEXT_SHOW_ALL_ACTIVE_MODIFIERS': 'View All Active Modifiers',
     'MENU_TEXT_ALL_ACTIVE_CONSTRUCTION_MODIFIERS': 'All Active Construction Modifiers',
 
+    'SHOP_NAME_ Construction_Skillcape': 'Construction_Skillcape',
+
+    'MODIFIER_DATA_farmingTreeSeedReturn': '+${value}% chance to regain a Tree Seed in Farming when harvested',
+    'MODIFIER_DATA_decreaseConstructionActionsToUpgrade': '-${value}% actions required to upgrade in Construction',
+    'MODIFIER_DATA_increaseConstructionActionsToUpgrade': '+${value}% actions required to upgrade in Construction',
+
     'DESCRIPTION_ADDS_ITEM': 'Gives ${itemQuantity} ${itemName} ${itemImage}',
 
     'TOASTS_MATERIALS_REQUIRED_TO_BUILD': 'You don\'t have the required materials to Build that.',
@@ -18,14 +24,42 @@ export const language = {
     'MASTERY_BONUS_ Construction_ 0': 'Each level provides +0.2% chance to preserve resources.',
     'MASTERY_BONUS_ Construction_ 1': '+5% chance to preserve resources.',
 
-    'SKILL_CATEGORY_ Construction_ Nails': 'Nails',
-    'SKILL_CATEGORY_ Construction_ Planks': 'Planks',
+    'SKILL_CATEGORY_ Construction_ Materials': 'Materials',
     'SKILL_CATEGORY_ Construction_ House': 'House',
 
     'CONSTRUCTION_ROOM_NAME_ Bedroom': 'Bedroom',
+    'CONSTRUCTION_ROOM_NAME_ Garden': 'Garden',
     'CONSTRUCTION_ROOM_NAME_ Kitchen': 'Kitchen',
+    'CONSTRUCTION_ROOM_NAME_ Laboratory': 'Laboratory',
+    'CONSTRUCTION_ROOM_NAME_ Outpost': 'Outpost',
+    'CONSTRUCTION_ROOM_NAME_ Refectory': 'Refectory',
+    'CONSTRUCTION_ROOM_NAME_ Training_Room': 'Training Room',
+    'CONSTRUCTION_ROOM_NAME_ Workshop': 'Workshop',
 
     'CONSTRUCTION_FIXTURE_OF_TIER': '${fixtureName}: Tier ${tier}',
 
+    'CONSTRUCTION_FIXTURE_NAME_ Apparatus': 'Apparatus',
+    'CONSTRUCTION_FIXTURE_NAME_ Archery_Range': 'Archery Range',
+    'CONSTRUCTION_FIXTURE_NAME_ Black_Market': 'Black Market',
+    'CONSTRUCTION_FIXTURE_NAME_ Desk': 'Desk',
+    'CONSTRUCTION_FIXTURE_NAME_ Dragon_Statue': 'Dragon Statue',
+    'CONSTRUCTION_FIXTURE_NAME_ Fire_Pit': 'Fire Pit',
+    'CONSTRUCTION_FIXTURE_NAME_ Flower_Pots': 'Flower Pots',
+    'CONSTRUCTION_FIXTURE_NAME_ Forge': 'Forge',
+    'CONSTRUCTION_FIXTURE_NAME_ Fridge': 'Fridge',
+    'CONSTRUCTION_FIXTURE_NAME_ Jungle_Gym': 'Jungle Gym',
+    'CONSTRUCTION_FIXTURE_NAME_ Lake': 'Lake',
+    'CONSTRUCTION_FIXTURE_NAME_ Mines': 'Mines',
+    'CONSTRUCTION_FIXTURE_NAME_ Orchard': 'Orchard',
+    'CONSTRUCTION_FIXTURE_NAME_ Oven': 'Oven',
+    'CONSTRUCTION_FIXTURE_NAME_ Prayer_Altar': 'Prayer Altar',
+    'CONSTRUCTION_FIXTURE_NAME_ Rune_Altar': 'Rune Altar',
+    'CONSTRUCTION_FIXTURE_NAME_ Spell_Library': 'Spell Library',
     'CONSTRUCTION_FIXTURE_NAME_ Storage_Chest': 'Storage Chest',
-}
+    'CONSTRUCTION_FIXTURE_NAME_ Telescope': 'Telescope',
+    'CONSTRUCTION_FIXTURE_NAME_ Tool_Rack': 'Tool Rack',
+    'CONSTRUCTION_FIXTURE_NAME_ Training_Dummy': 'Training Dummy',
+    'CONSTRUCTION_FIXTURE_NAME_ Trapping_Hut': 'Trapping Hut',
+    'CONSTRUCTION_FIXTURE_NAME_ Wardrobe': 'Wardrobe',
+    'CONSTRUCTION_FIXTURE_NAME_ Worktable': 'Worktable'
+}    
