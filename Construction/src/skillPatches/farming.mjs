e98ee528bf7a7fb031399d69e064eaa321e993b0
William Sullivan <william@symbotics.co.uk>
2024-10-01 20:11:09 -0400

    Typo

--- Construction/src/skillPatches/farming.mjs
+++ Construction/src/skillPatches/farming.mjs
@@ -1,6 +1,6 @@
 export function patchFarming(ctx) {
     ctx.patch(Farming, 'rollForAdditionalItems').after(function (_, rewards, growthTime, recipe){
-        if (recipe.category.id === "melvorD:Tree" && !disableSeedRefund) {
+        if (recipe.category.id === "melvorD:Tree") {
             const chance = this.game.modifiers.farmingTreeSeedReturn;
             if (rollPercentage(chance))
                 rewards.addItem(recipe.seedCost.item, 1);
